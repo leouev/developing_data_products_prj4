c54e36e0083ebac43e67e0152f346b9b7b9cab24
leouev <leouev@gmail.com>
2019-09-19 18:25:36 -0700

    second Update

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(shinyWidgets)
 library(shinythemes)
 library(plotly)
-
+library(leaflet)
 shinyUI(fluidPage(
         ## change the appearance of the shiny page
         theme = shinytheme("cerulean"),
@@ -29,7 +29,32 @@ shinyUI(fluidPage(
                                         plotOutput("out3"), plotOutput("out2")),
                                 hr(),plotOutput("out1")
                         )
+                ),
+                ## tab3
+                tabPanel("Documentation", br(),
+                         p(h3("List map")),
+                         br(),
+                         helpText("In the tab 'list map', the map shows the distribution of 2019 New York City's Airbnb lists,
+                                  The number in the map shows the list numbers in the area,
+                                  By clicking the numbers, you will zoom into that area,
+                                  When zoom to the 'Airbnb' logo, you can check the list name of the unit"),
+                         
+                         p(h3("Prices Insights")),
+                         br(),
+                         helpText("In the tab 'Price Insights', you will have a side panel to filter your choices.\n
+                                  In the slider you will choose the price range you care about.
+                                  In the five check boxes below, you can choose multiple neighbourhood area for comparison.
+                                  The dashboar panel on the right will show the data you choose.
+                                  "),
+                         p(h3("About")),
+                         br(),
+                         helpText("This website is generated using data extracted from New York City Airbnb Open Data
+                                   Airbnb listings and metrics in NYC, NY, USA (2019), more details could be find at
+                                   https://www.kaggle.com/dgomonov/new-york-city-airbnb-open-data
+                                  ")
                 )
+                
+                
                 
         )
         )
